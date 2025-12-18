4076d56785c093dc175d9b830f0ef26359c17277
renthinkbig <renampatt@gmail.com>
2025-12-18 16:11:03 +0530

    Update QR_authoriser_V1.1.py

--- QR_authoriser_V1.1.py
+++ QR_authoriser_V1.1.py
@@ -78,7 +78,7 @@ if captured_image is not None:
 
             # Crop QR code region
             qr_crop = img[y_min:y_max, x_min:x_max]
-            # st.image(qr_crop)
+            st.image(qr_crop)
             h, w = img.shape[:2]
 
             # Crop central region (say 60-70% of QR code)
